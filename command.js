0ffe8934091d7664e345cea2ae6bc4b3300bde7e
VipelyRS <theboi2017@gmail.com>
2021-07-18 13:24:57 -0400

    Fixed the command 'v!ping' again

--- command.js
+++ command.js
@@ -8,8 +8,8 @@ module.exports = (client, aliases, callback) => {
     client.on('message', message => {
         const { content } = message;
 
-        aliases.forEach(alies => {
-            const command = `${prefix}${alies}`
+        aliases.forEach(alias => {
+            const command = `${prefix}${alias}`
 
             if (content.startsWith(`${command} `) || content === command) {
                 console.log(`Running the command ${command}`)
