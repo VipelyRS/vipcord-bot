2ed4b00f8ca9081cec44ca1e71f79eae83d3438a
VipelyRS <theboi2017@gmail.com>
2021-07-18 11:27:35 -0400

    Reversed my Fuckup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
-//const client = new Discord.Client()
-const config = require('./config.json')
+const client = new Discord.Client()
+//const config = require('./config.json')
 
 client.on('ready', () => {
     console.log('The Client is Ready!')
