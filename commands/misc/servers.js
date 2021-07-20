a79ac4bdd7ff929e052d45761853b4a4c4968431
VipelyRS <theboi2017@gmail.com>
2021-07-20 13:27:30 -0400

    Fixed servers command

--- commands/misc/servers.js
+++ commands/misc/servers.js
@@ -1,9 +1,11 @@
+const client = new Discord.Client()
+
 module.exports = {
     commands: 'servers',
     minArgs: 0,
     maxArgs: 0,
     callback: (message, arguments, text) => {
-        guilds.cache.forEach((guilds) => {
+        client.guilds.cache.forEach((guilds) => {
             message.channel.send(`${guild.name} has a total of ${guilds.memberCount} members`)
         })
     },
