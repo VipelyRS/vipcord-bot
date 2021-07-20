69747520f4298b96c26667dff01fe1de12300381
VipelyRS <theboi2017@gmail.com>
2021-07-20 11:31:43 -0400

    Fixed a crash with requiring a file that doesn't exist

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const client = new Discord.Client()
 
 const config = require('./config.json')
 const { prefix, token } = require('./config.json')
-const command = require('./command')
 
 client.on('ready', () => {
     console.log('The Client is Ready!')
