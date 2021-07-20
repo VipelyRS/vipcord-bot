e790f0899c2c2570908864489727b2855598414b
VipelyRS <theboi2017@gmail.com>
2021-07-20 13:25:55 -0400

    Fixing servers command

--- commands/misc/servers.js
+++ commands/misc/servers.js
@@ -3,7 +3,7 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     callback: (message, arguments, text) => {
-        client.guilds.cache.forEach((guilds) => {
+        guilds.cache.forEach((guilds) => {
             message.channel.send(`${guild.name} has a total of ${guilds.memberCount} members`)
         })
     },
