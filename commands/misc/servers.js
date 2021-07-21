48e352b66bfa05e533a6b3409b7208a29165be2d
VipelyRS <theboi2017@gmail.com>
2021-07-21 13:38:26 -0400

    Trying to fix all of the commands that are broken

--- commands/misc/servers.js
+++ commands/misc/servers.js
@@ -6,7 +6,7 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     callback: (client, message, arguments, text) => {
-        client.guilds.cache.forEach((guilds) => {
+        client.guilds.cache.forEach((guild) => {
             message.channel.send(`${guild.name} has a total of ${guilds.memberCount} members`)
         })
     },
