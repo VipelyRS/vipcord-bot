6497dff6c00a71ca2155e44ba9a55f141af992b6
VipelyRS <theboi2017@gmail.com>
2021-09-20 23:49:07 -0400

    added intents, I think

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const { Discord, Intents } = require('discord.js')
-const client = new Discord.Client()
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const config = require('./config.json')
 const { prefix, token } = require('./config.json')
