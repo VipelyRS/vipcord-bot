9225f7127bf5afb6493df21495671a837b337eb4
VipelyRS <theboi2017@gmail.com>
2021-09-21 00:29:00 -0400

    maybe fixed it?

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const fs = require('fs')
 const Discord = require('discord.js')
-const { Intents } = require('discord.js')
-const client = new Discord.Client()
+const { Client, Intents } = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const config = require('./config.json')
 const { prefix, token } = require('./config.json')
