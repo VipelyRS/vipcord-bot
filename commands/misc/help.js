a80fe5cb135c1f3c3bbcc881b7909fefc87b038e
VipelyRS <theboi2017@gmail.com>
2021-09-20 23:41:31 -0400

    re

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -1,13 +1,13 @@
 const { prefix } = require('../../config.json')
 const { MessageEmbed, Channel, Intents } = require('discord.js')
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 module.exports = {
     commands: 'help',
     minArgs: 0,
     maxArgs: 0,
-    callback: (message, arguments, text) => {
+    callback: (message, arguments, text, Client) => {
         const helpEmbed = new MessageEmbed()
             .setColor('#1a09db')
             .setTitle('Vipbot Help')
