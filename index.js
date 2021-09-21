020c67595f6856cb4de9e42783972a5083865e9e
VipelyRS <theboi2017@gmail.com>
2021-09-20 23:46:36 -0400

    This is retarded

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const fs = require('fs')
-const Discord = require('discord.js')
+const { Discord, Intents } = require('discord.js')
 const client = new Discord.Client()
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const config = require('./config.json')
 const { prefix, token } = require('./config.json')
