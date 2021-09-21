4493f72124ba292023f4125741cf3e6e10096a64
VipelyRS <theboi2017@gmail.com>
2021-09-20 23:58:42 -0400

    reverted some changes cause it crashes even worse

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
-const { Client, Intents, Discord } = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { Discord, Intents } = require('discord.js')
+const client = new Discord.Client()
 
 const config = require('./config.json')
 const { prefix, token } = require('./config.json')
