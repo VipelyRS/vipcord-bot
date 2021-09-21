5c7d6f69174cdb96e587646c6bdc9dc11302f9e3
VipelyRS <theboi2017@gmail.com>
2021-09-20 23:16:39 -0400

    Embeded help command test

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -1,23 +1,14 @@
 const { prefix } = require('../../config.json')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     commands: 'help',
     minArgs: 0,
     maxArgs: 0,
     callback: (message, arguments, text) => {
-        message.channel.send(`
-        Available Commands:
-
-**${prefix}help** - Displays the help menu
-**${prefix}kick <@user>** - Kicks a targeted user
-**${prefix}ban <@user>** - Bans a targeted user
-**${prefix}purge** - Clears all messages in a channel
-**${prefix}prefix** - Changes the bot prefix
-**${prefix}servers** - Shows all the servers the bot is in
-**${prefix}serverinfo** - Displays info about the current server
-**${prefix}version** - Shows the bot version number
-*aliases: ${prefix}v*
-**${prefix}sugondese** - Nuts
-        `)
+        const helpEmbed = new MessageEmbed()
+            .setColor('#1a09db')
+            .setTitle('Vipbot Help')
+            .setDescription('Provides help for commands')
     },
 }
