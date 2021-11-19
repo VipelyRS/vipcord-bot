4eab446c7c8e11af702ffcc94ed78a6e8a5f5fb1
VipelyRS <theboi2017@gmail.com>
2021-11-19 03:44:34 -0500

    e

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const { Client, Intents } = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const config = require('./config.json')
 const { prefix, token } = require('./config.json')
